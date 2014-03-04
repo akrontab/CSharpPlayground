ccc6f58e025e7b6d16e83c0291e34c56899a8395
Aaron Krone <akrone@gmail.com>
2014-03-03 23:11:48 -0700

    ThowWpf Added
    Reference for the interview question: What's the difference between
    'throw' and 'throw ex' in C#?

---
+++ ThrowWpf/ThrowWPF/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Effects;
+
+namespace ThrowWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtBlk.BorderThickness = new Thickness(2);
+            txtBlk.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+        }
+
+        private void btnThrow_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                JustThrow();
+            }
+            catch (Exception ex)
+            {
+                txtBlk.Text = ex.StackTrace;
+            }
+        }
+
+        private void SomeBuggyCode()
+        {
+            throw new Exception("This is my Exception");
+
+
+        }
+
+        private void JustThrow()
+        {
+            try
+            {
+                SomeBuggyCode();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void btnThrowEx_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ThrowEx();
+            }
+            catch (Exception ex)
+            {
+                txtBlk.Text = ex.StackTrace;
+            }
+        }
+
+        private void ThrowEx()
+        {
+            try
+            {
+                SomeBuggyCode();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
